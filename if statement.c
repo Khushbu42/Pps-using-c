43a2f4290dc4041637d1cb2c05b98ac9b5d79051
Khushbu42 <khushbunarnaware42@gmail.com>
2025-05-05 15:59:57 +0530

    Create if statement.c

---
+++ if statement.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main( )
+{
+	int num;
+	printf(" enter the number: ");
+	scanf( " %d", &num);
+	if ( num > 3)
+	{
+	    printf( " yes\n");
+	}
+
+	printf("End of program.");
+	
+	return 0;
+	
+}
