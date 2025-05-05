9d50f34876a9bd42cfd7143f8c6d02db57d3c7ed
Khushbu42 <khushbunarnaware42@gmail.com>
2025-05-05 14:13:32 +0530

    Update and rename README.md to Sum.c

---
+++ Sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main( )
+{
+	int num1, num2, sum;
+
+	printf("enter first number:\t");
+	scanf("%d",& num1);
+	
+	printf("enter second number:\t");
+	scanf("%d",& num2);
+	
+	sum= num1+num2;
+	printf( " the sum is %d", sum);
+	
+	return 0;
+	
